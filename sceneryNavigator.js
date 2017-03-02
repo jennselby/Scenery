4fffc4d00aa1f551275a65bc9555829f921fbfda
Jen Selby <jselby@nuevaschool.org>
2017-03-02 09:32:42 -0800

    Got basic scene switching working

--- sceneryNavigator.js
+++ sceneryNavigator.js
@@ -2,33 +2,95 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Navigator,
+  TouchableHighlight,
   Text,
   View
 } from 'react-native';
 
+// Code based on example at https://facebook.github.io/react-native/docs/navigator.html
+
 export default class SceneryNavigator extends Component {
   render() {
+    const routes = [
+        {
+         text: 'You are in the woods, and you hear a river nearby, out of sight.',
+         nextText: 'Go to river',
+         nextScene: 1,
+        },
+        {
+         text: 'You are standing next to a river.',
+         nextText: 'Swim',
+         nextScene: 2,
+        },
+        {
+         text: 'You are swimming!',
+         nextText: '',
+         nextScene: undefined,
+        },
+
+    ];
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          TODO: Fill in this component.
-        </Text>
-      </View>
+        <Navigator
+            style={styles.navigator}
+            initialRoute={routes[0]}
+            renderScene={(route, navigator) => 
+                <Text style={styles.mainText}>{route.text}</Text>
+            }
+            navigationBar={
+                <Navigator.NavigationBar
+                    style={styles.navBar}
+                    routeMapper={{
+                        LeftButton: (route, navigator, index, navState) => {
+                            if (index === 0) {
+                                return null;
+                            } else {
+                                return (
+                                    <TouchableHighlight onPress={() => navigator.pop()}>
+                                        <Text style={styles.menuText}>Back</Text>
+                                    </TouchableHighlight>
+                                );
+                            }
+                        },
+                        RightButton: (route, navigator, index, navState) => {
+                            if (route.nextScene === undefined) {
+                                return null;
+                            } else {
+                                return (
+                                    <TouchableHighlight onPress={() => navigator.push(routes[route.nextScene])}>
+                                        <Text style={styles.menuText}>{route.nextText}</Text>
+                                    </TouchableHighlight>
+                                );
+                            }
+                        },
+                        Title:  (route, navigator, index, navState) => {
+                            return null;
+                        },
+                    }}
+                />
+            }
+        />
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  navigator: {
+    paddingTop: 100,
+    paddingRight: 20,
+    paddingLeft: 20,
+    backgroundColor: '#b4e6e8',
   },
-  welcome: {
+  mainText: {
     fontSize: 36,
     textAlign: 'center',
-    margin: 10,
+  },
+  menuText: {
+    fontSize: 24,
+    padding: 10,
+  },
+  navBar: {
+    backgroundColor: '#117377',
   },
 });
 
